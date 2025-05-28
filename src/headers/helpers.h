ce6cdb160fbe5e8dc035c8fc4206c642a96eb11d
RMalinowski <r.malinowski3211@gmail.com>
2025-05-28 18:41:56 +0200

    make helper funcion for connecting vectors

---
+++ src/headers/helpers.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "tree.h"
+
+#include <memory>
+#include <vector>
+
+template <typename Content>
+std::vector<Content> joinVectors(std::vector<Content> v1, std::vector<Content> v2){
+    v1.insert(v1.end(), v2.begin(), v2.end());
+
+    return v1;
+}
